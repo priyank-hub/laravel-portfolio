32f368101493cbf3e2b9fa94b42339dd0c63aa07
Priyank <patelpriyank177@gmail.com>
2021-11-16 16:30:36 -0500

    UI

--- resources/js/Pages/Admin/Dashboard.vue
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -9,6 +9,21 @@
                     <div class="col-6 col-md-4" style="text-align: right">
                         <span class="text-dark">{{ user.name }}</span>
                         <i class="fas fa-chevron-down mx-2"></i>
+
+                        <!-- <b-dropdown id="dropdown-1" class="m-md-2">
+                            <template #button-content>
+                                <div>
+                                    <span class="text-dark">{{ user.name }}</span>
+                                    <i class="fas fa-chevron-down mx-2"></i>
+                                </div>
+                            </template>
+                            <b-dropdown-item>First Action</b-dropdown-item>
+                            <b-dropdown-item>Second Action</b-dropdown-item>
+                            <b-dropdown-item>Third Action</b-dropdown-item>
+                            <b-dropdown-divider></b-dropdown-divider>
+                            <b-dropdown-item active>Active action</b-dropdown-item>
+                            <b-dropdown-item disabled>Disabled action</b-dropdown-item>
+                        </b-dropdown> -->
                     </div>
                 </div>
             </div>
